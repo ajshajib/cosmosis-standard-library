5ae0e1fe48e571adbab8c3a752a1799860e7d12e
Anowar Shajib <ajshajib@uchicago.edu>
2024-12-12 21:11:40 -0600

    update: add new configuration and script files for supernova analysis

---
+++ forecast_inis/sn_srd.py
@@ -0,0 +1,32 @@
+"""Demonstration of the use of the :class:`Supernova` statistics object."""
+
+import sacc
+import firecrown.likelihood.supernova as sn
+from firecrown.likelihood.gaussian import ConstGaussian
+from firecrown.likelihood.likelihood import NamedParameters
+
+
+def build_likelihood(params: NamedParameters):
+    """Build the Firecrown likelihood object.
+
+    We have no extra tools for this example.
+    """
+    # Here we instantiate the necessary statistic object to deal with SNIa data.
+    snia_stats = sn.Supernova(sacc_tracer="sn_ddf_sample")
+
+    # Here we instantiate the actual likelihood. The statistics argument carry
+    # the order of the data/theory vector.
+    lk = ConstGaussian(statistics=[snia_stats])
+
+    #    We load the correct SACC file.
+    saccfile = params.get_string("sacc_file")
+    sacc_data = sacc.Sacc.load_fits(saccfile)
+
+    # The read likelihood method is called passing the loaded SACC file, the
+    # statistic functions will receive the appropriated sections of the SACC
+    # file..
+    lk.read(sacc_data)
+
+    # This script will be loaded by the appropriated connector. The framework
+    # will call the factory function that should return a Likelihood instance.
+    return lk
