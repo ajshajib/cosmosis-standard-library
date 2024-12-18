f91e6952d29bb3b331ffc0d25ec1496559b90d39
Anowar Shajib <ajshajib@uchicago.edu>
2024-12-18 05:54:51 -0600

    update: add submit_all.sh script to streamline sbatch job submissions

---
+++ inis/submit_all.sh
@@ -0,0 +1,17 @@
+sbatch all.sbatch
+sbatch cmb_bao.sbatch
+sbatch cmb.sbatch
+sbatch bao.sbatch
+sbatch dessn.sbatch
+
+sbatch lcdm_all.sbatch
+sbatch lcdm_cmb_bao.sbatch
+sbatch lcdm_cmb.sbatch
+sbatch lcdm_bao.sbatch
+sbatch lcdm_dessn.sbatch
+
+sbatch all_lim.sbatch
+sbatch cmb_bao_lim.sbatch
+sbatch cmb_lim.sbatch
+sbatch bao_lim.sbatch
+sbatch dessn_lim.sbatch
