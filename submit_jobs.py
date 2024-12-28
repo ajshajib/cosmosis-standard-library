1d22bd9e570087447571fea5198da3c21e609db5
Anowar Shajib <ajshajib@uchicago.edu>
2024-12-27 19:36:42 -0600

    update: refine job submission parameters in submit_jobs.py to enable specific run configurations

--- submit_jobs.py
+++ submit_jobs.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-modifiers = [""]  # "_lim", "_lcdm"]  # ""
+modifiers = ["lim"]  # "_lim", "_lcdm"]  # ""
 
 # runs = ["wl", "sn", "sl", "bao", "bao_cmb", "cmb", "sl_bao", "all"]
 
@@ -9,9 +9,9 @@ modifiers = [""]  # "_lim", "_lcdm"]  # ""
 
 run_specs = [
     # ["all", 8],
-    # ["wl", 8],
+    ["wl", 8],
     # ["bao_cmb", 8],
-    ["cmb", 8],
+    # ["cmb", 8],
     # ["sl", 4],
     # ["sn_bao", 2],
     # ["sn", 2],
