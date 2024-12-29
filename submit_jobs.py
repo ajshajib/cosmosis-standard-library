0f0b3f17825e8cfa0ebca93922191afcc8a977f2
Anowar Shajib <ajshajib@uchicago.edu>
2024-12-29 08:37:40 -0600

    update: change output directory for files in modules.ini and samplers.ini; adjust job submission parameters in submit_jobs.py for improved performance

--- submit_jobs.py
+++ submit_jobs.py
@@ -1,21 +1,21 @@
 import os
 import time
 
-modifiers = ["_lim"]  # "_lim", "_lcdm"]  # ""
+modifiers = ["", "_lim", "_lcdm"]  # ""
 
 # runs = ["wl", "sn", "sl", "bao", "bao_cmb", "cmb", "sl_bao", "all"]
 
 # n_cores = [8, 2, 4, 2, 8, 8, 2, 8]
 
 run_specs = [
-    # ["all", 8],
-    ["wl", 8],
-    # ["bao_cmb", 8],
-    # ["cmb", 8],
-    # ["sl", 4],
-    # ["sn_bao", 2],
-    # ["sn", 2],
-    # ["bao", 2],
+    ["all", 16],
+    ["wl", 16],
+    ["bao_cmb", 16],
+    ["cmb", 16],
+    ["sl", 4],
+    ["sn_bao", 2],
+    ["sn", 2],
+    ["bao", 2],
 ]
 
 for mod in modifiers:
