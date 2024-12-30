1b3796ce8e764f575a05aa46a36641b19f4aef4d
Anowar Shajib <ajshajib@uchicago.edu>
2024-12-30 17:04:53 -0600

    update: uncomment memory allocation specification in submit_amd.sh for AMD job submission

--- submit_amd.sh
+++ submit_amd.sh
@@ -5,7 +5,8 @@
 #SBATCH --partition=amd
 #SBATCH --account=pi-jfrieman                                                          
 #SBATCH -t 36:00:00
-#SBATCH --cpus-per-task=1                                        
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G                                   
 #SBATCH --mail-user=ajshajib@rcc.uchicago.edu
 #SBATCH --mail-type=FAIL 
 
