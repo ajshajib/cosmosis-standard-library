85a35f7d96d2d2890c84c1c038ba925e81b74481
Anowar Shajib <ajshajib@uchicago.edu>
2024-12-30 23:56:08 -0600

    update: modify SLURM job specifications for AMD compatibility in forecast_sn.sh

--- forecast_sn.sh
+++ forecast_sn.sh
@@ -3,17 +3,17 @@
 #SBATCH --job-name="sn_forecast"
 #SBATCH --output=/home/ajshajib/Logs"/joblog.%j"
 #SBATCH --error=/home/ajshajib/Logs"/error.%j"
-#SBATCH --partition=caslake
+#SBATCH --partition=amd
 #SBATCH --account=pi-jfrieman                                                          
 #SBATCH -t 36:00:00      
-#SBATCH --ntasks=24
+#SBATCH --ntasks=48
 #SBATCH --cpus-per-task=1                                                          
 #SBATCH --mail-user=ajshajib@rcc.uchicago.edu                                   
 #SBATCH --mail-type=FAIL   
 
 source /home/ajshajib/.bashrc
-conda activate cosmosis
+conda activate cosmosis_amd
 source cosmosis-configure
 
 export OMP_NUM_THREADS=1
-mpirun -n 24 cosmosis --mpi /home/ajshajib/cosmosis-standard-library/forecast_inis/srd_sn.ini
+mpirun -n 48 cosmosis --mpi /home/ajshajib/amd-cosmosis-standard-library/forecast_inis/srd_sn.ini
