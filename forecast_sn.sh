2b9567f5f3b20809bcc7086fb1d53407ebb136a2
Anowar Shajib <ajshajib@uchicago.edu>
2025-01-21 11:28:50 -0600

    update: change SLURM partition and conda environment in forecast_sn.sh for improved compatibility

--- forecast_sn.sh
+++ forecast_sn.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="sn_forecast"
 #SBATCH --output=/home/ajshajib/Logs"/joblog.%j"
 #SBATCH --error=/home/ajshajib/Logs"/error.%j"
-#SBATCH --partition=amd
+#SBATCH --partition=caslake
 #SBATCH --account=pi-jfrieman                                                          
 #SBATCH -t 36:00:00      
 #SBATCH --ntasks=48
@@ -12,8 +12,8 @@
 #SBATCH --mail-type=FAIL   
 
 source /home/ajshajib/.bashrc
-conda activate cosmosis_amd
+conda activate cosmosis
 source cosmosis-configure
 
 export OMP_NUM_THREADS=1
-mpirun -n 48 cosmosis --mpi /home/ajshajib/amd-cosmosis-standard-library/forecast_inis/srd_sn.ini
+mpirun -n 48 cosmosis --mpi /home/ajshajib/cosmosis-standard-library/forecast_inis/srd_sn.ini
